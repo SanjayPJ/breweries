eb00cc7d24e3c318a858bb4eccbecd54ca30de8b
sanjaypj <sanjaypjayan2000@gmail.com>
2019-07-29 02:54:48 -0400

    breweries completed

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  clickCounter: number = 0;
+  name: string = "";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  countClick() {
+    this.clickCounter += 1;
+  }
+
+  setClasses() {
+    let classes = {
+      active: this.clickCounter > 4,
+      notactive: this.clickCounter < 4
+    }
+
+    return classes;
+  }
+}
